869c73122c1de1fa77716ca81fd300246cf6e27c
BeatrizMiotto <miottobiasforever@gmail.com>
2022-12-08 20:39:04 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World! My name is Beatriz!!!");
+
+namespace projeto_aula;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = 5;
+        var b = 7;
+        var c = a + b;
+        Console.WriteLine("A soma é, " + c);
+    }
+}
